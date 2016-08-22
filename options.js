1dafd59189210fda131abc170a9b0b602026cdf9
extra-mayo <zjz204@nyu.edu>
2016-08-21 21:03:35 -0400

    Initial files
    Contains common features

---
+++ options.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Jason on 8/19/2016.
+ */
+
+//initiate new array variables for each weekdays
+var Sunday = [], Monday = [], Tuesday = [], Wednesday = [], Thursday = [], Friday = [], Saturday = [];
+
+//store the weekdays into an array
+var weekDays = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
+
+function show (show, day, season, episode) {
+    this.nameOfShow = show;
+    this.day = day;
+    if (!season) {
+        this.season = "??";
+    }
+    else {
+        this.season = season;
+    }
+    if (!episode) {
+        this.episode = "??"
+    }
+    else {
+        this.episode = episode;
+    }
+}
+
+
+function convertDayToNumber(day){
+    var result;
+    var sunday = /^sunday/i,
+        monday = /^monday/i,
+        tuesday = /^tuesday/i,
+        wednesday = /^wednesday/i,
+        thursday = /^thursday/i,
+        friday = /^friday/i,
+        saturday = /^saturday/i;
+
+    switch (true){
+        case sunday.test(day): result = "0"; break;
+        case monday.test(day): result = "1"; break;
+        case tuesday.test(day): result = "2"; break;
+        case wednesday.test(day): result = "3"; break;
+        case thursday.test(day): result = "4"; break;
+        case friday.test(day): result = "5"; break;
+        case saturday.test(day): result = "6"; break;
+        default: console.log("no such day!");
+            result = false;
+    }
+    return result;
+
+}
+
+
+function saveShow(){
+    //store user's new show (or their input) into variable newShow
+    //TO-DO: implement more attributes
+    var newShow = new show($('#showName').val(),$('#day').val(), $('#season').val(), $('#episode').val());
+    if (!newShow){
+        alert("Error: no show or attributes(?) specified");
+        return;
+    }
+    else {
+        if (convertDayToNumber(newShow.day) === false){
+            console.log("invalid day! (-1)");
+        }
+        else {
+            weekDays[convertDayToNumber(newShow.day)].push(newShow);
+            console.log("day is converted to " + convertDayToNumber(newShow.day));
+            console.log( weekDays[convertDayToNumber(newShow.day)]);
+        }
+
+    }
+    chrome.storage.sync.set({"schedule": weekDays}, function() {
+        // Notify that we saved.
+        alert('Settings saved');
+        console.log(newShow);
+    });
+}
+var submitButton = document.querySelector('button.submit');
+submitButton.addEventListener('click', saveShow);
+
---
+++ popup.js
@@ -0,0 +1,154 @@
+/**
+ * Created by Jason on 8/19/2016.
+ */
+
+
+
+//Get weekday
+var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var date = new Date();
+var day = weekdays[date.getDay()];
+
+//Header of the popup
+function getFullWeekdayName(){
+    var weekDay = "<h1 id='title'>"
+        + day
+        + " (" + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + ")"
+        + "</h1>";
+    return weekDay;
+}
+
+function getPreviousDay(){
+
+}
+
+function getNextDay(){
+
+}
+
+function seasonLabel(todaysShow){
+    var seasonName = "(";
+    if (todaysShow.season < 10){
+        seasonName += "S0" + todaysShow.season;
+    }
+    else {
+        seasonName += "S" +todaysShow.season;
+    }
+
+    if (todaysShow.episode < 10){
+        seasonName += "E0" + todaysShow.episode;
+    }
+    else {
+        seasonName += "E" + todaysShow.episode;
+    }
+
+    seasonName += ")";
+    return seasonName;
+
+}
+
+
+// USE BOTTOM FOR REFERENCES
+// function outputDevicesToList(devices) {
+//     var table = $('<table border="1">');
+//     table.append($("<tr>" +
+//         "<th>" + "Name" + "</th>" +
+//         "<th>" + "OS" + "</th>" +
+//         "<th>" + "Id" + "</th>" +
+//         "<th>" + "Type" + "</th>" +
+//         "<th>" + "Chrome Version" + "</th>" +
+//         "</tr>"));
+//     for (i = 0; i < devices.length; i++) {
+//         table.append($("<tr>" +
+//             "<td>" + devices[i].name + "</td>" +
+//             "<td>" + devices[i].os + "</td>" +
+//             "<td>" + devices[i].id + "</td>" +
+//             "<td>" + devices[i].type + "</td>" +
+//             "<td>" + devices[i].chromeVersion + "</td>" +
+//             "</tr>"));
+//     }
+//     return table;
+// }
+
+function getFullWeekdayName(){
+    var weekDay = "<h1 id='title'>"
+        + day
+        + " (" + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + ")"
+        + "</h1>";
+    return weekDay;
+}
+
+function loadShow(){
+    var table = $('<table>');
+    var nothing = $("<p>");
+
+
+    chrome.storage.sync.get("schedule", (function(results) {
+
+        if (typeof results.schedule == "undefined"){
+            console.log("is it empty?:", typeof results.schedule == "undefined");
+            table.append($("<tr id='nothingRow'>" +
+                "<th>" + "You have nothing listed" + "</th>" +
+            "</tr>"));
+        }
+        else {
+            console.log("??");
+            var todaysShow = results.schedule[date.getDay()];
+            //if there's nothing in today's schedule
+            table.append($("<tr id='categoryRow'>" +
+                "<th>" + "List of Shows" + "</th>" +
+                "<th>" + "link" + "</th>" +
+                "</tr>"));
+            console.log("week schedule: ", results)
+            console.log("todays schedule:", todaysShow);
+            console.log("todays show total length: ", todaysShow.length);
+
+
+            for (i = 0; i < todaysShow.length; i++) {
+                table.append($("<tr class='showRow'>" +
+                    "<td>" + todaysShow[i].nameOfShow + " " + seasonLabel(todaysShow[i]) + "</td>"
+                    + "</tr>"));
+            }
+        }
+    }));
+
+    console.log(table);
+    return table;
+}
+
+
+function writeDay() {
+    $('#day').empty();
+    $('#day').append(getFullWeekdayName());
+    console.log(getFullWeekdayName());
+    $('#day').append(loadShow());
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    writeDay();
+});
+
+
+
+/*
+
+ storage.get('css', function(items) {
+ console.log(items);
+ // If there is CSS specified, inject it into the page.
+ if (items.css) {
+ chrome.tabs.insertCSS({code: items.css}, function() {
+ if (chrome.runtime.lastError) {
+ message.innerText = 'Not allowed to inject CSS into special page.';
+ } else {
+ message.innerText = 'Injected style!';
+ }
+ });
+ } else {
+ var optionsUrl = chrome.extension.getURL('options.html');
+ message.innerHTML = 'Set a style in the <a target="_blank" href="' +
+ optionsUrl + '">options page</a> first.';
+ }
+ });
+ */
