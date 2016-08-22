c9acd798b235e87ba0eaa14b4850ffc20d5dea09
extra-mayo <zjz204@nyu.edu>
2016-08-21 21:17:46 -0400

    removed unnecessary stuff

--- popup.js
+++ popup.js
@@ -54,28 +54,6 @@ function seasonLabel(todaysShow){
 }
 
 
-// USE BOTTOM FOR REFERENCES
-// function outputDevicesToList(devices) {
-//     var table = $('<table border="1">');
-//     table.append($("<tr>" +
-//         "<th>" + "Name" + "</th>" +
-//         "<th>" + "OS" + "</th>" +
-//         "<th>" + "Id" + "</th>" +
-//         "<th>" + "Type" + "</th>" +
-//         "<th>" + "Chrome Version" + "</th>" +
-//         "</tr>"));
-//     for (i = 0; i < devices.length; i++) {
-//         table.append($("<tr>" +
-//             "<td>" + devices[i].name + "</td>" +
-//             "<td>" + devices[i].os + "</td>" +
-//             "<td>" + devices[i].id + "</td>" +
-//             "<td>" + devices[i].type + "</td>" +
-//             "<td>" + devices[i].chromeVersion + "</td>" +
-//             "</tr>"));
-//     }
-//     return table;
-// }
-
 function getFullWeekdayName(){
     var weekDay = "<h1 id='title'>"
         + day
@@ -136,25 +114,3 @@ document.addEventListener('DOMContentLoaded', function () {
     writeDay();
 });
 
-
-
-/*
-
- storage.get('css', function(items) {
- console.log(items);
- // If there is CSS specified, inject it into the page.
- if (items.css) {
- chrome.tabs.insertCSS({code: items.css}, function() {
- if (chrome.runtime.lastError) {
- message.innerText = 'Not allowed to inject CSS into special page.';
- } else {
- message.innerText = 'Injected style!';
- }
- });
- } else {
- var optionsUrl = chrome.extension.getURL('options.html');
- message.innerHTML = 'Set a style in the <a target="_blank" href="' +
- optionsUrl + '">options page</a> first.';
- }
- });
- */
